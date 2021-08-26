94d32befb89ffbff404e5ea5718fc8323d29e424
Lucia <lucia.aznar1987@gmail.com>
2021-08-25 21:08:29 -0300

    Actualizacion de actividad

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./view/home.html"));
 });
 
-const puerto = process.env.PORT || 3000
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./view/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./view/login.html"));
+});
+
+const puerto = process.env.PORT || 3030
 
 app.listen(puerto, () => {
   console.log(`Server is running on PORT : ${puerto}`);
